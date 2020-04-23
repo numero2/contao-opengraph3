70b790d1302c0ba6214c56386418d8f9f79363a4
Benny Born <benny.born@numero2.de>
2020-04-23 13:51:33 +0200

    fixed #13

--- classes/OpenGraphIsotope.php
+++ classes/OpenGraphIsotope.php
@@ -3,12 +3,12 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2017 Leo Feyer
+ * Copyright (c) 2005-2020 Leo Feyer
  *
  * @package   OpenGraph3
  * @author    Benny Born <benny.born@numero2.de>
  * @license   LGPL
- * @copyright 2017 numero2 - Agentur für digitales Marketing
+ * @copyright 2020 numero2 - Agentur für digitales Marketing
  */
 
 
@@ -53,29 +53,34 @@ class OpenGraphIsotope {
             // add shipping weight
             if( isset($objProduct->shipping_weight) ) {
 
+                $weightProduct = NULL;
                 $weightProduct = Weight::createFromTimePeriod($objProduct->shipping_weight);
-                $weightMin = new Weight(1000,'g');
 
-                $objScale = new Scale();
-                $objScale->add($weightProduct);
+                if( $weightProduct ) {
 
-                // convert small weights to gram (g)
-                if( $objScale->isLessThan($weightMin) ) {
+                    $weightMin = new Weight(1000,'g');
 
-                    $convertedUnit = 'g';
-                    $convertedWeight = Unit::convert($weightProduct->getWeightValue(), $weightProduct->getWeightUnit(), 'g');
+                    $objScale = new Scale();
+                    $objScale->add($weightProduct);
 
-                // convert larger weights to kilogram (kg)
-                } else {
+                    // convert small weights to gram (g)
+                    if( $objScale->isLessThan($weightMin) ) {
 
-                    $convertedUnit = 'kg';
-                    $convertedWeight = Unit::convert($weightProduct->getWeightValue(), $weightProduct->getWeightUnit(), 'kg');
+                        $convertedUnit = 'g';
+                        $convertedWeight = Unit::convert($weightProduct->getWeightValue(), $weightProduct->getWeightUnit(), 'g');
+
+                        // convert larger weights to kilogram (kg)
+                    } else {
+
+                        $convertedUnit = 'kg';
+                        $convertedWeight = Unit::convert($weightProduct->getWeightValue(), $weightProduct->getWeightUnit(), 'kg');
+                    }
+
+                    $convertedWeight = number_format($convertedWeight, 2, '.', '');
+
+                    OpenGraph3::addProperty('og_product_shipping_weight_value',$convertedWeight,$objProduct);
+                    OpenGraph3::addProperty('og_product_shipping_weight_unit',$convertedUnit,$objProduct);
                 }
-
-                $convertedWeight = number_format($convertedWeight, 2, '.', '');
-
-                OpenGraph3::addProperty('og_product_shipping_weight_value',$convertedWeight,$objProduct);
-                OpenGraph3::addProperty('og_product_shipping_weight_unit',$convertedUnit,$objProduct);
             }
 
             OpenGraph3::addProperty('og_product_product_link',Environment::get('url') . Environment::get('requestUri'),$objProduct);
