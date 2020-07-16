f2a6331c9d7b18731fac02c678a9f7bf0568a7a8
Darko Seleši <hallo@w3scouts.com>
2020-07-16 17:45:25 +0200

    Update tl_page.php
    Added version compare

--- dca/tl_page.php
+++ dca/tl_page.php
@@ -23,13 +23,14 @@ $GLOBALS['TL_DCA']['tl_page']['palettes']['root'] = str_replace(
 ,   $GLOBALS['TL_DCA']['opengraph_fields']['palettes']['default'].'{dns_legend'
 ,   $GLOBALS['TL_DCA']['tl_page']['palettes']['root']
 );
-
-$GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback'] = str_replace(
-    '{dns_legend'
-,   $GLOBALS['TL_DCA']['opengraph_fields']['palettes']['default'].'{dns_legend'
-,   $GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback']
-);
-
+if (version_compare(VERSION, '4.9', '>='))
+{
+    $GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback'] = str_replace(
+        '{dns_legend'
+    ,   $GLOBALS['TL_DCA']['opengraph_fields']['palettes']['default'].'{dns_legend'
+    ,   $GLOBALS['TL_DCA']['tl_page']['palettes']['rootfallback']
+    );
+}
 $GLOBALS['TL_DCA']['tl_page']['palettes']['regular'] = str_replace(
     '{protected_legend'
 ,   $GLOBALS['TL_DCA']['opengraph_fields']['palettes']['default'].'{protected_legend'
