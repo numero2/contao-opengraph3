d57837a614aa8c208f02e953f9c61395026ac9e4
Benny Born <benny.born@numero2.de>
2021-03-09 15:05:02 +0100

    alternative fix for PR #17

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -8,7 +8,7 @@
  * @package   Opengraph3
  * @author    Benny Born <benny.born@numero2.de>
  * @author    Michael Bösherz <michael.boesherz@numero2.de>
- * @license   LGPL 
+ * @license   LGPL
  * @copyright 2021 numero2 - Agentur für digitales Marketing GbR
  */
 
